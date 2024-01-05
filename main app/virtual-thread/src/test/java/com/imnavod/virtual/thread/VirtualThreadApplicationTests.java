9bad561e2ac0f18e302f128e82aee639efeeb2b2
navoddilshan9 <navoddilshan9@gmail.com>
2024-01-06 03:15:14 +0530

    complete project commited

---
+++ main app/virtual-thread/src/test/java/com/imnavod/virtual/thread/VirtualThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imnavod.virtual.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
